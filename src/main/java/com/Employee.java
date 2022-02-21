2049033d96983fd4d043869a54d9badf21baabfe
seyha <sinseyha1x@gmail.com>
2022-02-21 09:15:47 +0700

    1

---
+++ src/main/java/com/Employee.java
@@ -0,0 +1,19 @@
+package com;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	private String name;
+	private EmployeeType employeeType;
+
+}
---
+++ src/main/java/com/EmployeeType.java
@@ -0,0 +1,8 @@
+package com;
+
+public enum EmployeeType {
+	FULL_TIME_EMPLOYEE,
+	PART_TIME_EMPLOYEE,
+	CONTRACT_EMPLOYEE,
+
+}
