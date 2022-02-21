883791c0389a427b6c6833afd2c399fba0b2cbbf
seyha <sinseyha1x@gmail.com>
2022-02-21 16:01:23 +0700

    commit

--- src/main/java/com/Company.java
+++ src/main/java/com/Company.java
@@ -1,5 +1,6 @@
 package com;
 
+import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,8 +12,8 @@ public class Company {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+	@Column
 	private String name;
-	
 	@Embedded
 	private Addresses addresses;
 
